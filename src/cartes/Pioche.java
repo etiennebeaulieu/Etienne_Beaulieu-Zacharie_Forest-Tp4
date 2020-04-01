9b41c74fd2da4b032cee510081aa34315ce0d80f
etiennebeaulieu_maison <etiennebeaulieu_maison@gmail.com>
2020-04-01 14:52:10 -0400

    Pioche et paquet

--- src/cartes/Pioche.java
+++ src/cartes/Pioche.java
@@ -8,27 +8,41 @@ public class Pioche
 	
 	public Pioche(PaquetDeCartes paquet)
 	{
-		
+		pioche = new Pile();
+		for(int i = paquet.size(); i> 0; i--)
+		{
+			pioche.empiler(paquet.consulterCarte(i));
+		}
 	}
 	
 	public Carte piger()
 	{
-		
+		Carte carte = null;
+		if(!pioche.isEmpty())
+		{
+			carte = (Carte)pioche.depiler();
+		}
+		return carte;
 	}
 	
 	public String consulterDessus() throws exception.PiocheException
 	{
-		
+		String carte = null;
+		if(!pioche.isEmpty())
+		{
+			carte = pioche.getPremier().toString();
+		}
+		return carte;
 	}
 	
 	public boolean isEmpty()
 	{
-		
+		return pioche.isEmpty();
 	}
 	
 	public int size()
 	{
-		
+		return pioche.size();
 	}
 	
 }
