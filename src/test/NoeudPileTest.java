8759328fd5c048699e80d71385e2d0f885173aea
ZacharieForest_maison <zacharie_forest@gmail.com>
2020-04-03 11:50:50 -0400

    et woila

--- src/test/NoeudPileTest.java
+++ src/test/NoeudPileTest.java
@@ -4,32 +4,40 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+import structures.pile.NoeudPile;
+
 public class NoeudPileTest
 {
-
+	NoeudPile n1, n2, nPrecedent;
+	Object obj1, obj2, obj3;
+	
 	@Before
 	public void testNoeudPileObject()
 	{
-		fail("Not yet implemented");
+		obj1 = new Object();
+		n1 = new NoeudPile(obj1);
 	}
 
-	@Test
-	public void testNoeudPileObjectInvalide()
-	{
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testNoeudPileObjectInvalide()
+//	{
+//		fail("Not yet implemented");
+//	}
 
 	@Before
 	public void testNoeudPileObjectNoeudPile()
 	{
-		fail("Not yet implemented");
+		obj2 = new Object();
+		obj3 = new Object();
+		nPrecedent = new NoeudPile(obj2);
+		n2 = new NoeudPile(obj3, nPrecedent);
 	}
 
-	@Test
-	public void testNoeudPileObjectNoeudPileInvalide()
-	{
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testNoeudPileObjectNoeudPileInvalide()
+//	{
+//		fail("Not yet implemented");
+//	}
 
 	@Test
 	public void testGetElement()
