8eb08197b3d632d7d4f8d7557bfea8ad3234c056
ZacharieForest_maison <zacharie_forest@gmail.com>
2020-04-03 12:01:07 -0400

    fu

--- src/test/NoeudPileTest.java
+++ src/test/NoeudPileTest.java
@@ -9,12 +9,12 @@ import structures.pile.NoeudPile;
 public class NoeudPileTest
 {
 	NoeudPile n1, n2, nPrecedent;
-	Object obj1, obj2, obj3;
+	Integer obj1, obj2, obj3;
 	
 	@Before
 	public void testNoeudPileObject()
 	{
-		obj1 = new Object();
+		obj1 = new Integer(5);
 		n1 = new NoeudPile(obj1);
 	}
 
@@ -27,8 +27,8 @@ public class NoeudPileTest
 	@Before
 	public void testNoeudPileObjectNoeudPile()
 	{
-		obj2 = new Object();
-		obj3 = new Object();
+		obj2 = new Integer(10);
+		obj3 = new Integer(15);
 		nPrecedent = new NoeudPile(obj2);
 		n2 = new NoeudPile(obj3, nPrecedent);
 	}
@@ -42,7 +42,7 @@ public class NoeudPileTest
 	@Test
 	public void testGetElement()
 	{
-		fail("Not yet implemented");
+		AssertEqual((Integer)n1.getElement(), obj1);
 	}
 
 	@Test
