88837896da8b1f2af89cb959bb97ead555a502e3
etiennebeaulieu_maison <etiennebeaulieu_maison@gmail.com>
2020-04-03 12:19:14 -0400

    PileTest

--- src/test/PileTest.java
+++ src/test/PileTest.java
@@ -3,62 +3,102 @@ package test;
 import static org.junit.Assert.*;
 
 import org.junit.*;
+import structures.pile.*;
 
 public class PileTest
 {
-
+	Pile p1;
+	
 	@Before
 	public void testPile()
 	{
-		fail("Not yet implemented");
+		p1 = new Pile();
 	}
 
-	@Test
-	public void testPileInvalide()
-	{
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testEmpiler()
 	{
-		fail("Not yet implemented");
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		
+		assertEquals(p1.getPremier(), new Integer(2));
+		assertEquals(p1.depiler(), new Integer(2));
+		assertEquals(p1.depiler(), new Integer(1));
 	}
 
 	@Test
 	public void testDepiler()
 	{
-		fail("Not yet implemented");
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		
+		assertEquals(p1.depiler(), new Integer(2));
+		assertEquals(p1.depiler(), new Integer(1));
 	}
 
 	@Test
 	public void testGetPremier()
 	{
-		fail("Not yet implemented");
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		
+		assertEquals(p1.getPremier(), new Integer(2));
 	}
 
 	@Test
 	public void testIsEmpty()
 	{
-		fail("Not yet implemented");
+		assertEquals(p1.isEmpty(), true);
+		p1.empiler(new Integer(1));
+		assertEquals(p1.isEmpty(), false);
 	}
 
 	@Test
 	public void testVider()
 	{
-		fail("Not yet implemented");
+		
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		
+		p1.vider();
+		assertEquals(p1.isEmpty(), true);
 	}
 
 	@Test
 	public void testSize()
 	{
-		fail("Not yet implemented");
+		assertEquals(p1.size(), 0);
+		
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		assertEquals(p1.size(), 2);
+		
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		assertEquals(p1.size(), 4);
+		
+		p1.vider();
+		assertEquals(p1.size(), 0);
+		
 	}
 
 	@Test
 	public void testToString()
 	{
-		fail("Not yet implemented");
+		assertEquals(p1.toString(), "vide!\n");
+		
+		p1.empiler(new Integer(1));
+		p1.empiler(new Integer(2));
+		p1.empiler(new Integer(3));
+		p1.empiler(new Integer(4));
+		p1.empiler(new Integer(5));
+		p1.empiler(new Integer(6));
+		assertEquals(p1.toString(), "6\n5\n4\n3\n2\n1\n");
 	}
 
 }
